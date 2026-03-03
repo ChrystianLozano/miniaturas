37319a87f2f5954e9d65d558951e48e3011fb885
Chrystian <chrystian@minidechrystian.lan>
2026-03-03 14:06:52 -0600

    Configure custom domain: miniatura.chrystian.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ChrystianLozano.github.io',
-  base: '/miniaturas',
+  site: 'https://miniatura.chrystian.dev',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
